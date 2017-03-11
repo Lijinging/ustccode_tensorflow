a2304cf2eab4c70c7f5fc42ddc7e003b4816f1b2
LIJING <lijinging@outlook.com>
2017-03-11 20:26:44 +0800

    修正

--- train.py
+++ train.py
@@ -1,30 +1,31 @@
 # coding:utf-8
-
-import scipy.io as sio
 import tensorflow as tf
-import numpy
+import scipy.io as scio
+import numpy as np
 
-train = sio.loadmat("data/dataTrain.mat")
-test = sio.loadmat("data/dataTest.mat")
-trainX = train['data'].astype('float32') / 255.
-testX = test['data'].astype('float32') / 255.
-trainY = numpy.zeros((trainX.shape[0], 32))
-trainY[numpy.array(range(train['label'].shape[0])), train['label'][:, 0]] = 1.
-testY = numpy.zeros((testX.shape[0], 32))
-testY[numpy.array(range(test['label'].shape[0])), test['label'][:, 0]] = 1.
+'''
+设置输入数据
+'''
+train_data_raw = scio.loadmat("data/dataTrain.mat")
+test_data_raw = scio.loadmat("data/dataTest.mat")
+# 数据归一化
+train_data = train_data_raw['data'].astype('float32') / 255.0
+test_data = test_data_raw['data'].astype('float32') / 255.0
+# label:
+train_label = np.zeros((train_data.shape[0], 32))
+test_label = np.zeros((test_data.shape[0], 32))
+train_label[np.array(range(train_data_raw['label'].shape[0])), train_data_raw['label'][:, 0]] = 1
+test_label[np.array(range(test_data_raw['label'].shape[0])), test_data_raw['label'][:, 0]] = 1
 
-X = tf.placeholder(tf.float32, [None, 400])
-Y_ = tf.placeholder(tf.float32, [None, 32])
 
-
-def weight_variable(shape, name="weight"):
+def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial, name=name)
+    return tf.Variable(initial)
 
 
-def bias_variable(shape, name="bias"):
+def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial, name=name)
+    return tf.Variable(initial)
 
 
 def conv2d(x, W):
@@ -36,60 +37,71 @@ def max_pool_2x2(x):
                           strides=[1, 2, 2, 1], padding='VALID')
 
 
-trainIndex = 0
+sess = tf.InteractiveSession()
 
+x = tf.placeholder(tf.float32, shape=[None, 400])
+y_ = tf.placeholder(tf.float32, shape=[None, 32])
 
-def next_batch(batch_size, data, label):
-    global trainIndex
-    i = trainIndex
-    if trainIndex + batch_size > data.shape[0]:
-        trainIndex = trainIndex + batch_size - data.shape[0]
-        return numpy.row_stack((data[i:], data[:trainIndex])), numpy.row_stack((label[i:], label[:trainIndex]))
-    else:
-        trainIndex += batch_size
-        return data[i:trainIndex], label[i:trainIndex]
+x_image = tf.reshape(x, [-1, 20, 20, 1])
 
-
-x_image = tf.reshape(X, [-1, 20, 20, 1])
-
-W_conv1 = weight_variable([5, 5, 1, 20],"W_conv1")
-b_conv1 = bias_variable([20],"b_conv1")
+W_conv1 = weight_variable([5, 5, 1, 20])  # 第一层卷积层
+b_conv1 = bias_variable([20])  # 第一层卷积层的偏置量
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+h_pool1 = max_pool_2x2(h_conv1)  # 第一次池化层
 
-W_conv2 = weight_variable([5, 5, 20, 50],"W_conv2")
-b_conv2 = bias_variable([50],"b_conv2")
+W_conv2 = weight_variable([5, 5, 20, 50])  # 第二次卷积层
+b_conv2 = bias_variable([50])  # 第二层卷积层的偏置量
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+h_pool2 = max_pool_2x2(h_conv2)  # 第二曾池化层
 
-W_ip1 = weight_variable([2 * 2 * 50, 500],"W_ip1")
-b_ip1 = bias_variable([500],"b_ip1")
+W_fc1 = weight_variable([2 * 2 * 50, 500])  # 全连接层
+b_fc1 = bias_variable([500])  # 偏置量
 h_pool2_flat = tf.reshape(h_pool2, [-1, 2 * 2 * 50])
-h_ip1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_ip1) + b_ip1)
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder("float")
-h_ip1_drop = tf.nn.dropout(h_ip1, keep_prob)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_ip2 = weight_variable([500, 32],"W_ip2")
-b_ip2 = bias_variable([32],"b_ip2")
-h_ip2 = tf.matmul(h_ip1_drop, W_ip2) + b_ip2
-y = tf.nn.softmax(h_ip2)
+W_fc2 = weight_variable([500, 32])
+b_fc2 = bias_variable([32])
+y = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-cross_entropy = -tf.reduce_sum(Y_ * tf.log(y))
-train_step = tf.train.AdamOptimizer(0.0001).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(Y_, 1))
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
-sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
-saver = tf.train.Saver()
+
+
+
+def next_batch(data, label, begin, length):
+    if begin >= data.shape[0]:
+        begin = begin % data.shape[0]
+    if begin + length > data.shape[0]:
+        add = next_batch(data, label, 0, length - (data.shape[0] - begin))
+        add[0] = np.row_stack((data[begin:], add[0]))
+        add[1] = np.row_stack((label[begin:], add[1]))
+    else:
+        add = [data[begin:begin + length], label[begin:begin + length]]
+    return add
 
 
 for i in range(3000):
-    batch_xs, batch_ys = next_batch(40, trainX, trainY)
+    size = 100
+    # batch = mnist.train.next_batch(100)
+    batch = next_batch(train_data, train_label, i * size, size)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
     if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={X: testX, Y_: testY, keep_prob: 1.0})
-        print("step %d, training accuracy %g" % (i, train_accuracy))
-    train_step.run(feed_dict={X: batch_xs, Y_: batch_ys, keep_prob: 1.0})
+        train_accuracy = accuracy.eval(feed_dict={
+            x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print(i, end=":")
+        print("test accuracy %g" % accuracy.eval(feed_dict={
+            x: test_data, y_: test_label, keep_prob: 1.0}))
 
-saver.save(sess,r"E:\workspace\python\mis.ustc.checkcode\model\model.ckpt")
+
+saver = tf.train.Saver()
+save_path = r"C:\Users\lijin\PycharmProjects\ustccode_tensorflow\model\model.ckpt"
+saver.save(sess, save_path)
+
+
+sess.close()
